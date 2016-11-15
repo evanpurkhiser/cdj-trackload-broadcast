b7ba9e4fbbf31b35070a429178241038a3e22eee
Evan Purkhiser <evanpurkhiser@gmail.com>
2016-11-15 04:06:24 -0800

    Cleanup websocket server a lot
     - Move the handler into a class so it may be configured
     - Add documentation
     - Add options parser

--- websocket-client.py
+++ websocket-client.py
@@ -1,55 +1,103 @@
 #!/usr/bin/env python3
 
-import sys
 import asyncio
 import websockets
 import mutagen
 import json
 import base64
 import os
+import signal
+import optparse
 
-MUSIC_PATH = '/home/evan/documents/multimedia/djing/tracks'
+class TrackloadWebsocketServer(object):
+    """
+    This is a websocket server that connects to the CDJ broadcast server to be
+    notified of when a track is loaded. The track details will be queried from
+    ID3 tags and sent to the web socket as JSON.
+    """
+    def __init__(self, music_path, websocket_server, broadcast_server):
+        """Initializes the trackload websocket server.q 
 
-def get_track_details(load_line):
-    deck_id, path = load_line.split(':', 1)
-    full_path = os.path.join(MUSIC_PATH, path)
+        - music_path:       Location of the music files on the moachine.
+        - websocket_server: Tuple of host and port for the websocket server.
+        - broadcast_server: Tuple of host and port of the broadcast server.
+        """
+        self.music_path = music_path
+        self.websocket_server = websocket_server
+        self.broadcast_server = broadcast_server
 
-    track = mutagen.File(full_path).tags
+    def __track_details(self, load_line):
+        """Given a track load line from the broadcast server, construct the
+        details of the loaded track.
 
-    art = track.getall('APIC')
+        - load_line: Should be in the format <deck_id>:<track_path>
+        """
+        deck_id, path = load_line.split(':', 1)
+        full_path = os.path.join(self.music_path, path)
 
-    # Convert the artwork into base 64
-    if len(art) > 0:
-        data = base64.b64encode(art[0].data).decode('utf-8')
-        mime = art[0].mime
+        track = mutagen.File(full_path).tags
 
-        artwork = 'data:{mime};base64,{data}'.format(mime=mime, data=data)
+        # Convert the artwork into base 64
+        art = track.getall('APIC')
+        artwork = None
 
-    return {
-        'deck_id': deck_id,
-        'artist':  track['TPE1'].text[0],
-        'title':   track['TIT2'].text[0],
-        'album':   track['TALB'].text[0] if 'TALB' in track else None,
-        'key':     track['TKEY'].text[0] if 'TKEY' in track else None,
-        'label':   track['TPUB'].text[0] if 'TPUB' in track else None,
-        'release': track['COMM::XXX'].text[0] if 'COMM::XXX' in track else None,
-        'year':    track['TDRC'].text[0].get_text() if 'TDRC' in track else None,
-        'artwork': artwork,
-    }
+        if len(art) > 0:
+            data = base64.b64encode(art[0].data).decode('utf-8')
+            mime = art[0].mime
 
-async def time(websocket, path):
-    reader, writer = await asyncio.open_connection('192.168.1.3', 19000)
+            artwork = 'data:{mime};base64,{data}'.format(mime=mime, data=data)
 
-    while True:
-        data = await reader.readline()
+        return {
+            'deck_id': int(deck_id),
+            'artist':  track['TPE1'].text[0],
+            'title':   track['TIT2'].text[0],
+            'album':   track['TALB'].text[0] if 'TALB' in track else None,
+            'key':     track['TKEY'].text[0] if 'TKEY' in track else None,
+            'label':   track['TPUB'].text[0] if 'TPUB' in track else None,
+            'release': track['COMM::XXX'].text[0] if 'COMM::XXX' in track else None,
+            'year':    track['TDRC'].text[0].get_text() if 'TDRC' in track else None,
+            'artwork': artwork,
+        }
 
-        load_line = data.decode().rstrip()
-        details = get_track_details(load_line)
-        details = json.dumps(details)
+    async def trackload(self, websocket, path):
+        """Coroutine that will wait for tracks to be loaded from the broadcast
+        server, promptly sending JSON to the websocket with information about
+        the track that was loaded.
+        """
+        reader, writer = await asyncio.open_connection(*self.broadcast_server)
 
-        await websocket.send(details)
+        while True:
+            data = await reader.readline()
 
-start_server = websockets.serve(time, 'localhost', 8765)
+            load_line = data.decode().rstrip()
+            details = self.__track_details(load_line)
+            details = json.dumps(details)
 
-asyncio.get_event_loop().run_until_complete(start_server)
-asyncio.get_event_loop().run_forever()
+            await websocket.send(details)
+
+    def start(self):
+        """Start the server in the asyncio event loop.
+        """
+        server = websockets.serve(self.trackload, *self.websocket_server)
+
+        asyncio.get_event_loop().run_until_complete(server)
+
+if __name__ == '__main__':
+    parser = optparse.OptionParser()
+    parser.add_option('-m', '--music-path', dest='music_path')
+
+    parser.add_option('-a', '--websocket-addr', dest='ws_host', default='localhost')
+    parser.add_option('-p', '--websocket-port', dest='ws_port', default=8765)
+
+    parser.add_option('-b', '--broadcast-addr', dest='bc_host')
+    parser.add_option('-c', '--broadcast-port', dest='bc_port', default=19000)
+
+    opts, _ = parser.parse_args()
+
+    ws_server  = (opts.ws_host, opts.ws_port)
+    bc_server  = (opts.bc_host, opts.bc_port)
+
+    tracklistServer = TrackloadWebsocketServer(opts.music_path, ws_server, bc_server)
+    tracklistServer.start()
+
+    asyncio.get_event_loop().run_forever()
